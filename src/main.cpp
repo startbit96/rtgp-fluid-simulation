6a7db0a5715e6b8ccee32ab5f8c39ca706d43163
Tim Schwarzbrunn <30576638+startbit96@users.noreply.github.com>
2023-06-25 23:03:57 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,6 @@
 
 int main(int argc, char* argv[]) 
 {
-    // Within the application we use relative paths to load objects or 
-    // to load the scene configuration.
-    // How can we ensure that the working directory is correct and the
-    // relative paths are working?
     rtgp_application();
     return 0;
 }
